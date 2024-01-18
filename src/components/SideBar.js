8bfe365ccfcba3daf2c39d254bb97b2d6b95ef30
Eduardo Noguez <eduno8a_ra@hotmail.com>
2024-01-17 20:32:13 -0600

    noguez responsive

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import logo from '../assets/logoibl.png'
+import home from '../assets/hogar.png'
+import profile from '../assets/avatar.png'
 import { Link } from "react-router-dom";
 
 
@@ -11,8 +13,8 @@ function SideBar() {
           <img className='login_logo' src={logo}/>
         </div>
       <ul>
-        <li> <Link to="/dashboard">Dashboard</Link> </li>
-        <li> <Link to="/profile">Profile</Link> </li>
+        <li> <Link to="/dashboard"> <img src={home} /> <span>Dashboard</span></Link> </li>
+        <li> <Link to="/profile"> <img src={profile} /> <span>Profile</span></Link> </li>
       </ul>
  
     </div>
